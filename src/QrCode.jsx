b885d87bf5091e83b31c13f7c4e55c4d7eddba52
fmfahath <fmfahath.dev@gmail.com>
2024-05-12 19:23:30 +0530

    getting user input

--- src/QrCode.jsx
+++ src/QrCode.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
-import qrImage from './assets/qr_code.svg'
+import React, { useState } from 'react'
+import sampleQrImage from './assets/qr_code.svg'
 
 const QrCode = () => {
+    const [qrImage, setImage] = useState(sampleQrImage);
+    const [inputData, setInputData] = useState('');
+    const [inputSize, setInputSize] = useState('');
+
+    const generateQR = () => {
+
+    }
+
     return (
         <div className="app-container">
             <div className="card">
@@ -12,22 +20,22 @@ const QrCode = () => {
                 <div className='form'>
                     <div className='form-element'>
                         <label htmlFor="dataInput" className='input-label'>Data for Code:</label>
-                        <input type="text" id='dataInput' placeholder='Enter data for QR' />
+                        <input type="text" id='dataInput' placeholder='Enter data for QR' onChange={(e) => setInputData(e.target.value)} />
                     </div>
                     <div className='form-element'>
                         <label htmlFor="sizeInput" className='input-label'>Image Size (e.g: 150px):</label>
-                        <input type="text" id='sizeInput' placeholder='Enter image size' />
+                        <input type="text" id='sizeInput' placeholder='Enter image size' onChange={(e) => setInputSize(e.target.value)} />
                     </div>
                     <div className='form-btn'>
-                        <button className='generate-btn'>Generate QR</button>
-                        <button className='download-btn'>Download</button>
+                        <button className='generate-btn' disabled={false} onClick={generateQR}>Generate QR</button>
+                        <button className='download-btn' >Download</button>
                     </div>
                     <div className='footer'>
                         <p>Designed By: <a href="#">FM. Fahath</a></p>
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
